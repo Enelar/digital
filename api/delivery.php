fb4ad6aff0c868fe5a5a40b0701f985eac74dd85
Kirill Berezin <enelar@develop-project.ru>
2014-01-11 20:15:52 +0300

    Example two way loading prices

--- api/delivery.php
+++ api/delivery.php
@@ -4,10 +4,45 @@ class delivery extends api
 {
   protected function Reserve()
   {
+    $prices = LoadModule('api', 'delivery');
     return array
     (
       "design" => "pages/delivery",
-      "result" => "content"
+      "result" => "content",
+      "data" => $prices->Prices()
     );
   }
+  
+  protected function Prices()
+  {
+    return array
+    (
+      "design" => "pages/delivery_prices",
+      "data" => array
+      (
+        "prices" => array
+        (
+          array
+          (
+            "title" => "В пределах КАД",
+            "price" => 250,
+            "timing" => "В день заказа/на следующий день"
+          ),
+          array
+          (
+            "title" => "Приоритетная",
+            "price" => 500,
+            "timing" => "В течении 6 часов"
+          ),          
+          array
+          (
+            "title" => "Пригород 20КМ",
+            "price" => 500,
+            "timing" => "На следующий день"
+          ),
+        )
+      )    
+    );
+  }
+  
 }
