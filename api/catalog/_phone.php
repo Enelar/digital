37f7d817406bc6489344bff43aaf7aa4c7e774d5
Kirill Berezin <enelar@develop-project.ru>
2014-06-28 13:25:05 -0400

    Support db setting show # Please enter the commit message for your changes. Lines starting

--- api/catalog/_phone.php
+++ api/catalog/_phone.php
@@ -14,7 +14,7 @@ class _phone extends api
   {
     $res = db::Query(
       "SELECT id, price, (quantity > 0) as available, view_weight
-       FROM phones.models WHERE vendor=(SELECT id FROM phones.vendor WHERE name=$1)
+       FROM phones.models WHERE show=true AND vendor=(SELECT id FROM phones.vendor WHERE name=$1)
        ORDER BY view_weight ASC",
       array($name));
     $phone = LoadModule('api', 'phone');
