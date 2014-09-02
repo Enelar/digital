21ace97831831c641971c965f0148b96747cfc38
Kirill Berezin <enelar@develop-project.ru>
2014-09-01 20:52:31 -0400

    Callus design completed

--- less.php
+++ less.php
@@ -9,6 +9,7 @@ $result = tempnam("/tmp", "googleit_");
 $query = 'lessc '.escapeshellarg("{$url}.less");
 
 header("Content-type: text/css");
+header("Cache-Control: public, max-age=60");
 $res = system($query);
 
 if ($res == false)
